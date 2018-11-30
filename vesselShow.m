61289a3571f04f23c599a26502c378be1f7df13f
Peikun Guo <50428519@qq.com>
2018-11-30 17:56:00 +0800

    store the tree in Cell format

--- vesselShow.m
+++ vesselShow.m
@@ -1,5 +1,6 @@
 function vesselShow(ROI)
-    [r, c, v] = ind2sub(size(ROI),find(ROI));
+% 3-D binarized matrix visualization
+    [r, c, v] = ind2sub(size(ROI), find(ROI));
     vessel = {[r, c, v]};
     coronary_show(vessel)
 end
